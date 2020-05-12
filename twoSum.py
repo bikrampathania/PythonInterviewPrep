6dff6f04e736a580765b125d8107c6e3e7672c7f
Bikramjeet Singh <cvichiee@protonmail.ch>
2020-05-12 23:47:33 +0530

    Revert "Revert "DynamicArray.py""
    This reverts commit 6dde5aa85b09e5d7516136d206036109f152a3a7.

---
+++ twoSum.py
@@ -0,0 +1,28 @@
+'''
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+'''
+
+def twoSum(arr, target):
+
+	if len(num) < 2:
+		return False
+
+	seen = {}
+
+	for index, value in enumerate(arr):
+		diff = target - value
+		if diff in seen:
+			return [seen[diff], index]
+		else:
+			seen[value] = index
+
+	return False
