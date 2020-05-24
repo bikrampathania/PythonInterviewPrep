e41df3a4147de19e0f1fc0bf8f0d134582afaf46
Bikramjeet Singh <cvichiee@protonmail.ch>
2020-05-24 23:53:28 +0530

    Implement a singly linked list

--- SinglyLinkedList.py
+++ SinglyLinkedList.py
@@ -4,20 +4,20 @@ Implement a basic Singly Linked List.
 In a singly linked list, we have an ordered list of items as individual Nodes that have pointers to other Nodes.
 '''
 
-class Node(object):
+class SLLNode(object):
 
 	def __init__(self, value):
 		self.value = value
-		self.nextnode = None
+		self.next_node = None
 
 #Now we can build out Linked List with the collection of nodes.
 
-a = Node(1)
-b = Node(2)
-c = Node(3)
+a = SLLNode(1)
+b = SLLNode(2)
+c = SLLNode(3)
 
-a.nextnode = b
-b.nextnode = c
+a.next_node = b
+b.next_node = c
 
 '''
 -> a.value
