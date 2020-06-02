a7a323cc7f5996c941c0234f88df0f6b321ab28c
Bikramjeet Singh <cvichiee@protonmail.ch>
2020-06-02 23:13:06 +0530

    Recursive function to sum all digits in integer n

--- SumRecursion2_UD.py
+++ SumRecursion2_UD.py
@@ -4,7 +4,7 @@ For example: if n = 4321, return 4+3+2+1
 '''
 
 def recSum2(n):
-	if len(str(n)) == 0:
+	if len(str(n)) == 1:
 		return n # base case
 	else:
 		return n%10 + recSum2(n//10) # recursive case
