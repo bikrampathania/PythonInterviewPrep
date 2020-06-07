b319b1fc4417c430e2afc80d8acbc69e14c2aa37
Bikramjeet Singh <cvichiee@protonmail.ch>
2020-06-07 19:00:01 +0530

    Write memoized factorial function

---
+++ FactorialMemoized.py
@@ -0,0 +1,18 @@
+'''
+Write memoized factorial function
+'''
+
+# create cache for known results
+
+
+factorial_memo = {}
+
+def factorial(k):
+
+	if k < 2:
+		return 1
+
+	if k not in factorial_memo:
+		factorial_memo[k] = k * factorial(k - 1)
+
+	return factorial_memo[k]
