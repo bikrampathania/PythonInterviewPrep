60f277b70e973028338df9fb56d2bd1167ff152a
Bikramjeet Singh <cvichiee@protonmail.ch>
2020-06-08 12:15:16 +0530

    Write function to output string permutations using recursion

---
+++ StringPermutationRecursion.py
@@ -0,0 +1,20 @@
+'''
+Given a string, write a function that uses recursion to output a list
+of all the possible permutations of that string.
+
+For example, given s = 'abc' the function should return ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+'''
+
+def permute(s):
+
+	out = []
+
+	if len(s) == 1:
+		out = [s]
+
+	for i, letter in enumerate(s):
+
+		for perm in permute(s[:i] + s[i+1:]):
+			out += [letter + perm]
+
+	return out
