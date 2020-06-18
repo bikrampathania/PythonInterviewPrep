baf2637bf0b66da2cf629f8385c04ebc5e3cd04b
Bikramjeet Singh <cvichiee@protonmail.ch>
2020-06-18 20:50:39 +0530

    Converting test keys to factory format

---
+++ UcnToFactory.py
@@ -0,0 +1,7 @@
+'''
+Prereq: Take test keys from Usn list and put them into a .txt file with each key on new line
+'''
+
+file = open('/Users/username/Downloads/list.txt', 'r').read().split('\n')
+for test in file: # take each test key from the text file
+    print('{"' + test + '"},') #. and convert it to factory format
